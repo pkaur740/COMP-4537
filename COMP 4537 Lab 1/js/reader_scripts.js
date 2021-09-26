38e34cf196627d4079cbed345ae930d488d9800b
Tommy-81321 <56455799+Tommy-81321@users.noreply.github.com>
2021-09-26 01:48:11 -0700

    update the reader page

--- COMP 4537 Lab 1/js/reader_scripts.js
+++ COMP 4537 Lab 1/js/reader_scripts.js
@@ -1,26 +1,33 @@
 const msg_notSupported = "Sorry, web storage is not supported!";
 
+if (typeof(Storage) == "undefined"){
+    document.write(msg_notSupported);
+    window.stop();
+}
+
+let notes = localStorage.getItem("notes");
+notes = JSON.parse(notes);
+// console.log(notes);
+for (let i = 0; i < notes.length; i++){
+    const parentDiv = document.createElement("div");
+    const elementId = "ele" + i;
+    parentDiv.setAttribute("id", elementId);
+    const txtarea = document.createElement("textarea");
+    txtarea.setAttribute("id", "txtarea" + i);
+    parentDiv.appendChild(txtarea);
+    document.body.appendChild(parentDiv);
+}
+
+
+for (let i = 0; i < notes.length; i++){
+    document.getElementById("txtarea" + i).value = notes[i][i];
+}
+
 setInterval(function(){
-    if (typeof(Storage) == "undefined"){
-        document.write(msg_notSupported);
-        window.stop();
-    }
-
-    let notes = localStorage.getItem("notes");
-    notes = JSON.parse(notes);
-    console.log(notes);
-
-    for (let i = 0; i < notes.length; i++){
-        const eleDiv = document.createElement("div");
-        eleDiv.setAttribute("id", "eleDiv");
-
-        let note = document.createTextNode(notes[i][i]);
-
-        eleDiv.appendChild(note);
-
-        document.body.appendChild(eleDiv);
-    }
-
-
-
+    cache_clear()
 }, 2000);
+
+
+function cache_clear() {
+    window.location.reload(true);
+  }
